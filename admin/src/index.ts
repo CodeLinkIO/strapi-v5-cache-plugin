63eb1c0e5ea35510e0ab17fa2562d7bed9d42622
Trung Nguyen <trung.nguyen@codelink.io>
2026-06-25 20:58:07 +0700

    [Fix] Permission fix

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -5,22 +5,27 @@ import PurgeEntityButton from './components/PurgeEntityButton';
 
 export default {
   register(app: any) {
+    console.log('[Strapi Cache] Registering');
     app.registerPlugin({
       id: PLUGIN_ID,
       initializer: Initializer,
       isReady: false,
       name: PLUGIN_ID,
     });
+    console.log('[Strapi Cache] Plugin Registered!');
 
     app.getPlugin('content-manager').injectComponent('listView', 'actions', {
       name: PurgeCacheButton,
       Component: PurgeCacheButton,
     });
+    console.log('[Strapi Cache] PurgeCacheButton Injected!');
 
     app.getPlugin('content-manager').injectComponent('editView', 'right-links', {
       name: PurgeEntityButton,
       Component: PurgeEntityButton,
     });
+
+    console.log('[Strapi Cache] PurgeEntityButton Injected!');
   },
 
   async registerTrads({ locales }: { locales: string[] }) {
